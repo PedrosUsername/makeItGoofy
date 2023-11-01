30c127b924f787dfc7c94d5adbe8c9bd09f7e8b5
Pedro <pilique11@gmail.com>
2023-11-01 01:44:55 -0300

    get working transitions

---
+++ README.md
@@ -0,0 +1,9 @@
+- node v16.20.2
+- npm v6.14.18
+- ffmpeg v n4.3.1
+- gl-transition-scripts v1.18.0
+
+run with:
+- node script.js
+
+then wait for output.mp4
--- script.js
+++ script.js
@@ -5,24 +5,26 @@ const exec = util.promisify(require('child_process').exec);
 const ephemeralFile = './tmp';
 const transitionsFile = './transitions'
 const framesFile = `${ephemeralFile}/frames`;
-const inputImg = `${ephemeralFile}/image.png`;
+const inputImg = `${ephemeralFile}/hfclub.jpeg`;
 const possibleTransitions = fs.readdirSync(transitionsFile);
 const goofyOutput = 'out.mp4';
 
 const step1 = `rm -rf ${framesFile}`;
 
-let step2Size = 7;
+let step2Size = 10;
 let step2Piece = '';
 while (step2Size > 0) {
     const randIndex = Math.floor(Math.random() * possibleTransitions.length);
     const transitionInput = `${transitionsFile}/${possibleTransitions[randIndex]}`;
+    possibleTransitions.splice(randIndex, 1);
 
     step2Piece = step2Piece + ` -t ${transitionInput} -i ${inputImg}`;
     step2Size = step2Size - 1;
 }
 
-const step2 = `gl-transition-render -g ./tmp/spiral.png -d 15 -i ${inputImg} ${step2Piece} -f 30 -w 632 -h 632 -o ${framesFile}`;
-const step3 = `ffmpeg -y -framerate 30 -i ${framesFile}/%d.png -r 30 ${goofyOutput}`;
+const step2 = `gl-transition-render -g ./tmp/spiral.png -d 15 -i ${inputImg} ${step2Piece} -f 30 -w 1280 -h 894 -o ${framesFile}`;
+const step3 = `ffmpeg -y -framerate 30 -i ${framesFile}/%d.png -r 30 ${framesFile}/video.mp4`;
+const step4 = `ffmpeg -y -i ${framesFile}/video.mp4 -c:v h264 -c:a aac -b:v 196k -b:a 196k -preset fast -crf 22 -ar 44100 -pix_fmt yuv420p -video_track_timescale 90000 -r 30 ${goofyOutput} `;
 
 async function makeItGoofy() {
 
@@ -39,6 +41,9 @@ async function makeItGoofy() {
 
         await exec(step3);
         console.log(`- step 3: ok`);
+
+        await exec(step4);
+        console.log(`- step 4: ok`);        
     } catch (err) {
 
         console.warn(err);
